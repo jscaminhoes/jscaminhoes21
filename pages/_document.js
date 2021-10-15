4bef592fad48a995bd50e39d8de863e3149a4b95
Igor Hoffmann Becker <igorhoffmannbecker10@gmail.com>
2021-10-15 19:11:51 -0300

    4.0 google ads config

--- pages/_document.js
+++ pages/_document.js
@@ -28,18 +28,10 @@ class MyDocument extends Document {
             src="https://www.googletagmanager.com/gtag/js?id=UA-210274657-1"
           />
           <script>
-         
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-210274657-1">
-</script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){
-  dataLayer.push(arguments);
-  }
-  gtag('js', new Date());
-
-  gtag('config', 'UA-210274657-1');
-</script>
+            <script
+              async
+              src="https://www.googletagmanager.com/gtag/js?id=UA-210274657-1"
+            />
           </script>
         </Head>
         <body>
