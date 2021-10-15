977f7d62d317c7428f3ed7f9e37a0d6e48566c5b
Igor Hoffmann Becker <igorhoffmannbecker10@gmail.com>
2021-10-15 19:04:39 -0300

    3.0 google ads config

--- pages/_document.js
+++ pages/_document.js
@@ -33,7 +33,9 @@ class MyDocument extends Document {
 </script>
 <script>
   window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
+  function gtag(){
+  dataLayer.push(arguments);
+  }
   gtag('js', new Date());
 
   gtag('config', 'UA-210274657-1');
